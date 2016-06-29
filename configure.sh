6db3e4e363c92f3ca928f3d2e4120e488946467f
vinicius <vinicius.esufg@gmail.com>
2016-06-29 12:23:04 -0300

    creating an interface-service mapping

--- configure.sh
+++ configure.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 INSTALLER_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-XMPP_IP=$(/sbin/ifconfig eth1 | grep 'inet end.:' | cut -d: -f2 | awk '{ print $1}')
-BROKER_AMQP_IP=$(/sbin/ifconfig eth0 | grep 'inet end.:' | cut -d: -f2 | awk '{ print $1}')
 
-if [ -z "$XMPP_IP" -a "$XMPP_IP" == " " ]; then
-    XMPP_IP=$(/sbin/ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-fi
-
-if [ -z "$BROKER_AMQP_IP" -a "$BROKER_AMQP_IP" == " " ]; then
-    BROKER_AMQP_IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-fi
+BROKER_INTERFACE="eth0"
+AMQP_INTERFACE="eth1"
+XMPP_INTERFACE="eth1"
 
 add_hosts_config() {
     oldIFS=$IFS
@@ -18,16 +12,51 @@ add_hosts_config() {
     while read line; do
         IFS=', ' read -r -a array <<< "$line"
         if [[ ${array[0]} != *"#"* ]]; then
-            echo "${array[1]}   ${array[0]}" >> $INSTALLER_HOME/testbed-files/root/hosts
+            echo -e "${array[1]}\t${array[0]}" >> $INSTALLER_HOME/testbed-files/root/hosts
         fi
         IFS=$'n'
     done < $INSTALLER_HOME/conf/nodes.conf
     IFS=$old_IFS
 }
 
+set_service_interface() {
+    oldIFS=$IFS
+
+    while read line; do
+        IFS='=' read -r -a array <<< "$line"
+        if [[ ${array[0]} = *"<broker>"* ]]; then
+            BROKER_INTERFACE=${array[1]}
+        elif [[ ${array[0]} = *"<amqpserver>"* ]]; then
+            AMQP_INTERFACE=${array[1]}
+        elif [[ ${array[0]} = *"<xmppserver>"* ]]; then
+            XMPP_INTERFACE=${array[1]}
+        fi
+        IFS=$'n'
+    done < $INSTALLER_HOME/conf/interface-service-map.conf
+    IFS=$old_IFS
+}
+
 set_ips() {
-    find $INSTALLER_HOME/testbed-files -type f -exec sed -i "s/<broker>/$BROKER_AMQP_IP/g" {} +
-    find $INSTALLER_HOME/testbed-files -type f -exec sed -i "s/<amqpserver>/$BROKER_AMQP_IP/g" {} +
+    set_service_interface
+
+    XMPP_IP=$(/sbin/ifconfig $XMPP_INTERFACE | grep 'inet end.:' | cut -d: -f2 | awk '{ print $1}')
+    BROKER_IP=$(/sbin/ifconfig $BROKER_INTERFACE | grep 'inet end.:' | cut -d: -f2 | awk '{ print $1}')
+    AMQP_IP=$(/sbin/ifconfig $AMQP_INTERFACE | grep 'inet end.:' | cut -d: -f2 | awk '{ print $1}')
+
+    if [ -z "$XMPP_IP" -a "$XMPP_IP" == " " ]; then
+        XMPP_IP=$(/sbin/ifconfig $XMPP_INTERFACE | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+    fi
+
+    if [ -z "$BROKER_IP" -a "$BROKER_IP" == " " ]; then
+        BROKER_IP=$(/sbin/ifconfig $BROKER_INTERFACE | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+    fi
+
+    if [ -z "$AMQP_IP" -a "$AMQP_IP" == " " ]; then
+        AMQP_IP=$(/sbin/ifconfig $AMQP_INTERFACE | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+    fi
+
+    find $INSTALLER_HOME/testbed-files -type f -exec sed -i "s/<broker>/$BROKER_IP/g" {} +
+    find $INSTALLER_HOME/testbed-files -type f -exec sed -i "s/<amqpserver>/$AMQP_IP/g" {} +
     find $INSTALLER_HOME/testbed-files -type f -exec sed -i "s/<xmppserver>/$XMPP_IP/g" {} +
 }
 

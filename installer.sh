946b49decd95bbe185eafb7d4b47b3e2e374c9d1
vinicius <vinicius.esufg@gmail.com>
2016-06-30 20:42:47 -0300

    fixing docker installation

--- installer.sh
+++ installer.sh
@@ -163,11 +163,12 @@ install_docker() {
         rm -rf /etc/apt/sources.list.d/docker.list
     fi
 
+    apt-get install -y --force-yes apt-transport-https ca-certificates
+    apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
     echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+
     apt-get update \
-    && apt-get install -y --force-yes apt-transport-https ca-certificates \
-    && apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D \
-    && apt-get update \
     && apt-get purge -y --force-yes lxc-docker \
     && apt-cache policy docker-engine \
     && apt-get install -y --force-yes linux-image-extra-$(uname -r) \
@@ -198,8 +199,10 @@ download_baseline_image() {
 }
 
 install_testbed() {
-    $INSTALLER_HOME/configure.sh
     install_dependencies
+
+    $INSTALLER_HOME/configure.sh
+
     install_docker
     install_docker_compose
     install_amqp_server

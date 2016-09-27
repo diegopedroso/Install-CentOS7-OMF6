babcc5f99dc78959216ec4c6735594202d3fb686
Vinícius Gonçalves Braga <vinicius.esufg@gmail.com>
2016-09-27 15:17:30 -0300

    fix a bug in reading the nodes.conf file

--- bin/resources/create_resources_input_file.py
+++ bin/resources/create_resources_input_file.py
@@ -19,7 +19,7 @@ all_nodes = ""
 for idx, line in enumerate(nodes_file):
     line = line.rstrip()
     if not line.startswith("#") and line:
-        node_parts = line.split(", ")
+        node_parts = line.split(",")
         node = resources_template.replace("<name>", node_parts[0])
         node = node.replace("<ip-if0>", node_parts[1])
         node = node.replace("<mac-if0>", node_parts[2])
@@ -27,7 +27,7 @@ for idx, line in enumerate(nodes_file):
         node = node.replace("<ip-cmc>", node_parts[4])
         node = node.replace("<mac-cmc>", node_parts[5])
         node = node.replace("<domain>", node_parts[6])
-        node = node.replace("<ip-gateway>", node_parts[7].rstrip())
+        node = node.replace("<ip-gateway>", node_parts[7].strip())
 
         all_nodes += node + ",\n"
 
